698e6af2e55b7215b5a698afb84e8398c2e3d406
xycu <smonindemid@gmail.com>
2026-08-19 23:05:35 +0500

    feat: Add a new developer

--- src/store/team.js
+++ src/store/team.js
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useTeamStore = defineStore('team', () => {
-  const members = ref([
-    {
+export const useTeamStore = defineStore('team',{
+  state: () => ({
+    selectedRole: 'all',
+    members: [
+      {
     id: 0,
     avatar: "/xycu2.jpg",
     title: "Xycu2",
     text: " Front-end developer. I transfer Figma mockups pixel-for-pixel using SCSS. I'm currently studying JavaScript in-depth, and my next big goal is mastering Vue.js.",
     link: "https://t.me/the_JavaScript",
     class: "btn-dark",
+    role: 'frontend'
   },
   {
     id: 1,
@@ -18,16 +21,44 @@ export const useTeamStore = defineStore('team', () => {
     text: "I'm a Developer, I love Programming apps on Flutter Favorite programming languages are Python, Dart, Java",
     link: "#",
     class: "btn-info",
+    role: 'backend'
   },
   {
     id: 2,
+    avatar: "/djifffb.png",
+    title: "djifffb",
+    text: "I'm a backend developer and love my job. I work on a lot of cool and awesome projects.",
+    link: "#",
+    class: "btn-success",
+    role: 'backend'
+  },
+  {
+    id: 3,
     avatar: "/korsan.jpg",
     title: "Korsany",
     text: "friend kosmonavt001",
     link: "#",
     class: "btn-success",
-  }
-  ])
+    role: 'backend'
+  },
+    ]
+  }),
+  getters: {
 
-  return { members }
+    filteredDevs: (state) => {
+
+      if (state.selectedRole === 'all') {
+        return state.members
+      }
+
+      return state.members.filter(dev => dev.role === state.selectedRole)
+    }
+  },
+
+  actions: {
+    setRole(role) {
+      this.selectedRole = role
+    }
+  }
+
 })
